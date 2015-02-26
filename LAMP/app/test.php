2f2be6adde79d57cff4aa8777a38a8ea15b136ef
Pankaj Malhotra(:bitgeeky) <devpankaj1994@gmail.com>
2015-02-26 23:46:09 +0530

    testing mysql

--- LAMP/app/test.php
+++ LAMP/app/test.php
@@ -1,1 +1,9 @@
-<?php  Print "Hello, World!"; ?>
+<?php
+echo "MySQL Test.";
+$link = mysql_connect('172.31.5.255', 'apache', 'password');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
+?>
--- LAMP/ops/terraform/lamp.tf
+++ LAMP/ops/terraform/lamp.tf
@@ -1,27 +1,26 @@
 provider "aws" {
-    access_key = "YOUR_ACCESS_KEY"
-    secret_key = "YOUR_SECRET_KEY"
+    access_key = ""
+    secret_key = ""
     region = "us-east-1"
 }
 
 resource "atlas_artifact" "php" {
-    name = "<username>/apache-php"
+    name = "username/apache-php"
     type = "aws.ami"
 }
 
 resource "atlas_artifact" "mysql" {
-    name = "<username>/mysql"
+    name = "username/mysql"
     type = "aws.ami"
 }
-
-resource "aws_security_group" "allow_all" {
-  name = "allow_all"
+resource "aws_security_group" "allow_tcp" {
+  name = "allow_tcp"
     description = "Allow all inbound traffic"
 
   ingress {
       from_port = 0
       to_port = 65535
-      protocol = "all"
+      protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -29,7 +28,7 @@ resource "aws_security_group" "allow_all" {
 resource "aws_instance" "php" {
     instance_type = "t2.micro"
     ami = "${atlas_artifact.php.metadata_full.region-us-east-1}"
-    security_groups = ["${aws_security_group.allow_all.name}"]
+    security_groups = ["${aws_security_group.allow_tcp.name}"]
 
     # This will create 1 instance
     count = 1
@@ -41,7 +40,7 @@ resource "aws_instance" "php" {
 resource "aws_instance" "mysql" {
     instance_type = "t2.micro"
     ami = "${atlas_artifact.mysql.metadata_full.region-us-east-1}"
-    security_groups = ["${aws_security_group.allow_all.name}"]
+    security_groups = ["${aws_security_group.allow_tcp.name}"]
 
     # This will create 1 instances
     count = 1
